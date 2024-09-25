5203662dca5bf4080cf21e080a95ec3c139e78b8
hbybyyang <2451759073@qq.com>
2024-09-25 09:14:24 +0800

    优化检查未使用变量的规则

--- src/index.ts
+++ src/index.ts
@@ -37,12 +37,14 @@ export var ts安全性: Linter.Config = {
     '@typescript-eslint/explicit-function-return-type': ['error', {}],
 
     // 检查没有使用的变量
+    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
-        varsIgnorePattern: '^_',
         argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
         destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       },
     ],
   },
