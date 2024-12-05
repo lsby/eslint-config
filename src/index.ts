678c02565ff00fd02764289a87147821f7d12f79
hbybyyang <2451759073@qq.com>
2024-12-06 07:46:18 +0800

    增加禁止使用取反

--- src/index.ts
+++ src/index.ts
@@ -59,8 +59,12 @@ export var ts安全性: Linter.Config = {
     // 永远使用 === 而不是 ==, 在一些情况下能自动修复
     eqeqeq: ['error', 'always'],
 
-    // 在除了逻辑表达式的地方, 使用使用??而不是||
+    // 在除了逻辑表达式的地方, 使用??而不是||
     '@typescript-eslint/prefer-nullish-coalescing': 'error',
+
+    // 禁止使用取反
+    // 对于 number | null 的值x, if(!x)在x等于null和0时都会触发, 这可能是非预期的
+    '@lsby/no-negation': 'error',
   },
 }
 
